2df62ac1c3a98b8e2e259a1efbcaf6917942fd86
Aleja-Carbajal <00253720@uca.edu.sv>
2021-03-16 22:32:02 -0600

    Added gitignore

---
+++ 02_1503Cocina_Clase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_1503Cocina_Clase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
