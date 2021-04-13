2dfd21f3670bc150d36da826aeac64554281cfc5
Aleja-Carbajal <00253720@uca.edu.sv>
2021-04-13 04:47:41 -0600

    help

---
+++ Ramdom List Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ramdom_List_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numRandList = new List<int>();
+            var randNum = new Random();
+
+            for (int i = 0; i < 10; i++)
+                numRandList.Add(randNum.Next(1,30));
+            
+            Console.WriteLine();
+            Console.WriteLine("--Lista de numeros aleatorios--");
+            Console.WriteLine();
+            numRandList.ForEach(num => Console.WriteLine(num));
+
+            Console.WriteLine();
+            Console.WriteLine("--Eliminando numeros primos de la lista...");
+
+            Predicate<int> CheckIfPrime = (int num) =>
+            {
+                for(int i = 2; i < num; i++)
+                {
+                    if(num % 2 == 0)
+                        return false;
+                }
+                return true;
+            };
+
+            numRandList.RemoveAll(CheckIfPrime);
+            numRandList.ForEach(num => Console.WriteLine(num));
+        }
+    }
+}
